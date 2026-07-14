5f3d520c1d73a2c8e3ebd4e942e151ca6c8b39a8
ZhuRan <zhuran@ZhuRandeMacBook-Air.local>
2026-07-14 21:33:02 +0800

    reward change2

--- source/robotlab_go2w_him/robotlab_go2w_him/tasks/locomotion/velocity/config/go2w/rough_env_cfg.py
+++ source/robotlab_go2w_him/robotlab_go2w_him/tasks/locomotion/velocity/config/go2w/rough_env_cfg.py
@@ -235,7 +235,7 @@ class UnitreeGo2WRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.rewards.is_terminated.weight = 0
 
         # Root penalties
-        self.rewards.lin_vel_z_l2.weight = -1.5
+        self.rewards.lin_vel_z_l2.weight = -1.0
         self.rewards.ang_vel_xy_l2.weight = -0.05
         self.rewards.flat_orientation_l2.weight = -0.5
         self.rewards.base_height_l2.weight = -5.0
