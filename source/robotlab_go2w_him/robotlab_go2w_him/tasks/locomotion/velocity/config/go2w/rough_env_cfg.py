9ec28a9e778d09fd2f6b7ba485f8d60e6f3534e4
Zeroran <2246188375@qq.com>
2026-07-14 23:38:58 +0800

    reward for baseline2_resume

--- source/robotlab_go2w_him/robotlab_go2w_him/tasks/locomotion/velocity/config/go2w/rough_env_cfg.py
+++ source/robotlab_go2w_him/robotlab_go2w_him/tasks/locomotion/velocity/config/go2w/rough_env_cfg.py
@@ -238,7 +238,7 @@ class UnitreeGo2WRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.rewards.lin_vel_z_l2.weight = -1.0
         self.rewards.ang_vel_xy_l2.weight = -0.05
         self.rewards.flat_orientation_l2.weight = -0.5
-        self.rewards.base_height_l2.weight = -5.0
+        self.rewards.base_height_l2.weight = -8.0
         self.rewards.base_height_l2.params["target_height"] = 0.40
         self.rewards.base_height_l2.params["asset_cfg"].body_names = [self.base_link_name]
         self.rewards.body_lin_acc_l2.weight = 0
@@ -264,9 +264,9 @@ class UnitreeGo2WRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.rewards.joint_vel_limits.params["asset_cfg"].joint_names = self.wheel_joint_names
         self.rewards.joint_power.weight = -2e-5
         self.rewards.joint_power.params["asset_cfg"].joint_names = self.leg_joint_names
-        self.rewards.stand_still.weight = -2.0
+        self.rewards.stand_still.weight = -2.4
         self.rewards.stand_still.params["asset_cfg"].joint_names = self.leg_joint_names
-        self.rewards.hip_default_l2.weight = -1.0
+        self.rewards.hip_default_l2.weight = -1.2
         self.rewards.hip_default_l2.params["asset_cfg"].joint_names = [".*_hip_joint"]
         self.rewards.run_still_no_wheels_l1.weight = -0.05
         self.rewards.run_still_no_wheels_l1.params["asset_cfg"].joint_names = self.leg_joint_names
@@ -295,17 +295,17 @@ class UnitreeGo2WRoughEnvCfg(LocomotionVelocityRoughEnvCfg):
         self.rewards.track_ang_vel_z_exp.weight = 1.8
 
         # Others
-        self.rewards.feet_air_time.weight = 1.5
+        self.rewards.feet_air_time.weight = 2.0
         self.rewards.feet_air_time.params["threshold"] = 0.4
         self.rewards.feet_air_time.params["sensor_cfg"].body_names = [self.foot_link_name]
         self.rewards.feet_contact.weight = 0
         self.rewards.feet_contact.params["sensor_cfg"].body_names = [self.foot_link_name]
         self.rewards.feet_contact_without_cmd.weight = 0.1
         self.rewards.feet_contact_without_cmd.params["sensor_cfg"].body_names = [self.foot_link_name]
-        self.rewards.feet_stumble.weight = -0.05
+        self.rewards.feet_stumble.weight = -0.25
         self.rewards.feet_stumble.params["sensor_cfg"].body_names = [self.foot_link_name]
-        self.rewards.feet_stumble.params["force_ratio"] = 3.0
-        self.rewards.feet_slide.weight = -0.1
+        self.rewards.feet_stumble.params["force_ratio"] = 2.0
+        self.rewards.feet_slide.weight = -0.25
         self.rewards.feet_slide.params["sensor_cfg"].body_names = [self.foot_link_name]
         self.rewards.feet_slide.params["asset_cfg"].body_names = [self.foot_link_name]
         self.rewards.feet_height.weight = 0
